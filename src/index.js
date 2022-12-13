6167b28d4e0d809c84a867123b43dd4123b2fd60
BigThunderSR <17056173+BigThunderSR@users.noreply.github.com>
2022-12-13 15:12:07 -0600

    Changed timeout to a value that seems to work better

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const onstarConfig = {
     checkRequestStatus: _.get(process.env, 'ONSTAR_SYNC', 'true') === 'true',
     refreshInterval: parseInt(process.env.ONSTAR_REFRESH) || (30 * 60 * 1000), // 30 min
     requestPollingIntervalSeconds: parseInt(process.env.ONSTAR_POLL_INTERVAL) || 6, // 6 sec default
-    requestPollingTimeoutSeconds: parseInt(process.env.ONSTAR_POLL_TIMEOUT) || 60, // 60 sec default
+    requestPollingTimeoutSeconds: parseInt(process.env.ONSTAR_POLL_TIMEOUT) || 90, // 60 sec default
     allowCommands: _.get(process.env, 'ONSTAR_ALLOW_COMMANDS', 'true') === 'true'
 };
 logger.info('OnStar Config', {onstarConfig});
